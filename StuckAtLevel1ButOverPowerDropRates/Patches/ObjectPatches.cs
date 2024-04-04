a26f4ecf4aeeb1181fe44abbf8df6af6f7b44b73
Jesse Vang <jessevang@sbcglobal.net>
2024-04-03 20:09:31 -0700

    Updated the code so that it works with stardew valley v1.6 Updated code to use new gamelocation.monsterdrop code and updated compiler from framework 5.0 to 6.0

--- StuckAtLevel1ButOverPowerDropRates/Patches/ObjectPatches.cs
+++ StuckAtLevel1ButOverPowerDropRates/Patches/ObjectPatches.cs
@@ -37,34 +37,29 @@ internal class ObjectPatches : BasePatcher
         
 
         NetCollection<Debris> debris = new NetCollection<Debris>();
-        IList<int> objects = monster.objectsToDrop;
+        IList<String> objects = monster.objectsToDrop;
         Vector2 playerPosition = new Vector2(Game1.player.GetBoundingBox().Center.X, Game1.player.GetBoundingBox().Center.Y);
         List<Item> extraDrops = monster.getExtraDropItems();
-        if (Game1.player.isWearingRing(526))
+        if (who.isWearingRing("526") && DataLoader.Monsters(Game1.content).TryGetValue(monster.Name, out var result))
         {
-            // Add this logic to original code to run code to run loot 20 times and adds item to list of items to drop
-           for (int i = 0; i < 24; i++) { 
-                string result1 = "";
-                Game1.content.Load<Dictionary<string, string>>("Data\\Monsters").TryGetValue(monster.Name, out result1);
-                if (result1 != null && result1.Length > 0)
+            // Add this logic to original code to run code to run loot 24 times and adds item to list of items to drop
+            for (int k = 0; k < 24; k++)
+            {
+                string[] objectsSplit = ArgUtility.SplitBySpace(result.Split('/')[6]);
+                for (int i = 0; i < objectsSplit.Length; i += 2)
                 {
-                    string[] objectsSplit = result1.Split('/')[6].Split(' ');
-                    for (int l = 0; l < objectsSplit.Length; l += 2)
+                    if (Game1.random.NextDouble() < Convert.ToDouble(objectsSplit[i + 1]))
                     {
-                        if (Game1.random.NextDouble() < Convert.ToDouble(objectsSplit[l + 1]))
-                        {
-                            objects.Add(Convert.ToInt32(objectsSplit[l]));
- 
-
-                        }
+                        objects.Add(objectsSplit[i]);
                     }
                 }
             }
         }
 
+
         else {
             //Add this logic to original code to run code to run loot 10 times and adds item to list of items to drop
-            for (int i = 0; i < 12; i++)
+            for (int j = 0; j < 12; j++)
             {
                 string result1 = "";
                 Game1.content.Load<Dictionary<string, string>>("Data\\Monsters").TryGetValue(monster.Name, out result1);
@@ -75,7 +70,7 @@ internal class ObjectPatches : BasePatcher
                     {
                         if (Game1.random.NextDouble() < Convert.ToDouble(objectsSplit[l + 1]))
                         {
-                            objects.Add(Convert.ToInt32(objectsSplit[l]));
+                            objects.Add(objectsSplit[l]);
 
 
                         }
@@ -86,18 +81,17 @@ internal class ObjectPatches : BasePatcher
 
 
         // code creates the items and drops onto screen.
-        for (int k = 0; k < objects.Count; k++)
+        List<Debris> debrisToAdd = new List<Debris>();
+        for (int i = 0; i < objects.Count; i++)
         {
-            int objectToAdd = objects[k];
-            if (objectToAdd < 0)
+            string objectToAdd = objects[i];
+            if (objectToAdd != null && objectToAdd.StartsWith('-') && int.TryParse(objectToAdd, out var parsedIndex))
             {
-                debris.Add(monster.ModifyMonsterLoot(new Debris(Math.Abs(objectToAdd), Game1.random.Next(1, 4), new Vector2(x, y), playerPosition)));
-
+                debrisToAdd.Add(monster.ModifyMonsterLoot(new Debris(Math.Abs(parsedIndex), Game1.random.Next(1, 4), new Vector2(x, y), playerPosition)));
             }
             else
             {
-                debris.Add(monster.ModifyMonsterLoot(new Debris(objectToAdd, new Vector2(x, y), playerPosition)));
-
+                debrisToAdd.Add(monster.ModifyMonsterLoot(new Debris(objectToAdd, new Vector2(x, y), playerPosition)));
             }
         }
 
